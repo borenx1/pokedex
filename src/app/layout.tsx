a6c88a52d26769c3b78a45a41fa24eede509e6ca
Boren Xue <53597966+borenx1@users.noreply.github.com>
2024-08-31 12:46:59 +1200

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import './globals.css';
 import QueryProvider from '@/providers/QueryProvider';
 import theme from '@/theme';
 
@@ -38,6 +41,12 @@ export default function RootLayout({
                 </Toolbar>
               </AppBar>
               {children}
+              <Box component="footer" sx={{ mt: 'auto', pt: 8, pb: 4, px: 4 }}>
+                <Typography variant="body2" align="center">
+                  Made with the help of{' '}
+                  <Link href="https://pokeapi.co/">PokeAPI</Link>
+                </Typography>
+              </Box>
             </ThemeProvider>
           </AppRouterCacheProvider>
         </QueryProvider>
